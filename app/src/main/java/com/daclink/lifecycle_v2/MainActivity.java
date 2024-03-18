ef6c2f08a1dbafc98de1e25792d173a859db26a4
Dr. Drew Clinkenbeard <dclinkenbeard@csumb.edu>
2024-03-18 09:40:01 -0700

    removed an unnecessary line.

--- app/src/main/java/com/daclink/lifecycle_v2/MainActivity.java
+++ app/src/main/java/com/daclink/lifecycle_v2/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate(Bundle) called");
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
